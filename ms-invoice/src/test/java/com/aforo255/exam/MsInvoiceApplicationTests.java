acc4b412eda1154ce0926f8a351f5eeac51b1658
Miguel Liberato <miguel.liberato.carmin@gmail.com>
2021-09-25 08:43:00 -0500

    invoice

---
+++ ms-invoice/src/test/java/com/aforo255/exam/MsInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aforo255.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
